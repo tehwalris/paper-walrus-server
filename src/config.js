00b9bb74525c2c06e6ec471555570c29642341c7
Philippe Voinov <philippevoinov@gmail.com>
2016-06-08 16:13:02 +0200

    Image upload (wip).

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ const os = require('os'),
   path = require('path');
 
 const port = 3000;
-const storePath = path.join(os.homedir(), '.paperWalrus/store.json');
+const basePath = path.join(os.homedir(), '.paperWalrus')
+const imagePath = path.join(basePath, 'images');
+const storePath = path.join(basePath, 'store.json');
 
-module.exports = {port, storePath};
+module.exports = {port, basePath, imagePath, storePath};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 const server = require('http').createServer(),
   io = require('socket.io')(server),
   StorePersister = require('./StorePersister'),
+  ImageStorage = require('./ImageStorage'),
   config = require('./config'); 
 
 const storePersister = new StorePersister(config.storePath);
+const imageStorage = new ImageStorage(config.imagePath);
 const store = storePersister.getStore();
 
 io.on('connection', function(socket){
@@ -15,6 +17,13 @@ io.on('connection', function(socket){
   socket.on('findItems', function(options, cb){
     console.log(options.tagIds);
     cb(store.findItems(options.tagIds));
+  });
+  socket.on('upload', function(image, options, cb) {
+    console.log('upload called', image, options);
+    imageStorage.create(image).then(id => {
+      console.log('image created', id);
+      cb(id);
+    }).catch(() => cb(null));
   });
   socket.on('disconnect', function(){
     console.log('client disconnnected');
