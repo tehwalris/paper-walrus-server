f623178a02f49d4eedc4f3f487fbfd9e51636e27
Philippe Voinov <philippevoinov@gmail.com>
2016-06-20 20:51:53 +0200

    Create directory for file uploads.

--- src/Uploader.js
+++ src/Uploader.js
@@ -1,8 +1,11 @@
 const multer = require('multer'),
   uuid = require('uuid').v4,
-  mime = require('mime');
+  mime = require('mime'),
+  mkdirp = require('mkdirp');
 
 module.exports = function (destination, allowedMimeTypes) {
+  mkdirp(destination, err => {if(err) throw err});
+
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, destination);
